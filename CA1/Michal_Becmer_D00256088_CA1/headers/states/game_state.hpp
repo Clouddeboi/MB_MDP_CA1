fbf1771c416e4d3baa8330bf94f4068c6bdf8e1e
Michal Becmer <93706431+Clouddeboi@users.noreply.github.com>
2025-09-21 21:31:03 +0100

    ✨ Created an abstract base class for the game states

--- CA1/Michal_Becmer_D00256088_CA1/headers/states/game_state.hpp
+++ CA1/Michal_Becmer_D00256088_CA1/headers/states/game_state.hpp
@@ -1,1 +1,16 @@
+//Author: Michal Becmer - D00256088
 #pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace states {
+	//Abstract base class for different game states
+	class GameState {
+	public:
+		virtual ~GameState() = default;
+
+		virtual void HandleEvent(const sf::Event& event) = 0;
+		virtual void Update(float deltaTime) = 0;
+		virtual void Render(sf::RenderWindow& window) = 0;
+	};
+}
---
+++ CA1/Michal_Becmer_D00256088_CA1/src/states/game_state.cpp
@@ -0,0 +1,4 @@
+//Author: Michal Becmer - D00256088
+#include "../../headers/states/game_state.hpp"
+
+//No implementation needed for abstract class.
